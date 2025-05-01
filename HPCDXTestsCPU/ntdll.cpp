d9db220ace26131b8d821eb46c8f02473874b089
user <user@user>
2025-05-01 18:24:49 +0000

    ntdll internals base done.

--- HPCDXTestsCPU/ntdll.cpp
+++ HPCDXTestsCPU/ntdll.cpp
@@ -1,2 +1,31 @@
 #include "ntdll_internal.h"
 #include "ntdll_internal_funcs.h"
+
+NTSTATUS __stdcall LdrGetDllHandle(WCHAR* DllPath, PULONG DllCharacteristics, PUNICODE_STRING DllName, PVOID* DllHandle)
+{
+	return STATUS_SUCCESS;
+}
+void __stdcall RtlInitUnicodeString(PUNICODE_STRING DestinationString, PCWSTR SourceString)
+{
+
+}
+NTSTATUS __stdcall RtlInitAnsiStringEx(_STRING* DestinationString, const char* SourceString)
+{
+	return STATUS_SUCCESS;
+}
+NTSTATUS __stdcall RtlAnsiStringToUnicodeString(PUNICODE_STRING DestinationString, _STRING* SourceString, BOOLEAN AllocateDestinationString)
+{
+	return STATUS_SUCCESS;
+}
+void __stdcall RtlFreeUnicodeString(PUNICODE_STRING UnicodeString)
+{
+
+}
+void __stdcall RtlSetLastWin32Error(ULONG LastError)
+{
+
+}
+ULONG __stdcall RtlNtStatusToDosError(NTSTATUS Status)
+{
+	return 0;
+}
--- HPCDXTestsCPU/ntdll_internal_funcs.h
+++ HPCDXTestsCPU/ntdll_internal_funcs.h
@@ -1,3 +1,10 @@
 #pragma once
 
 _PEB* NtCurrentPeb();
+NTSTATUS __stdcall LdrGetDllHandle(WCHAR* DllPath, PULONG DllCharacteristics, PUNICODE_STRING DllName, PVOID* DllHandle);
+void __stdcall RtlInitUnicodeString(PUNICODE_STRING DestinationString, PCWSTR SourceString);
+NTSTATUS __stdcall RtlInitAnsiStringEx(_STRING* DestinationString, const char* SourceString);
+NTSTATUS __stdcall RtlAnsiStringToUnicodeString(PUNICODE_STRING DestinationString, _STRING* SourceString, BOOLEAN AllocateDestinationString);
+void __stdcall RtlFreeUnicodeString(PUNICODE_STRING UnicodeString);
+void __stdcall RtlSetLastWin32Error(ULONG LastError);
+ULONG __stdcall RtlNtStatusToDosError(NTSTATUS Status);
