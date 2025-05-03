bc799bcc14b3fea3450970f85ed5011fd34b4458
user <user@user>
2025-05-03 18:34:26 +0000

    Find pattern refactor.

--- HPCDXTestsCPU/pch.cpp
+++ HPCDXTestsCPU/pch.cpp
@@ -1,102 +1,55 @@
-// pch.cpp: source file corresponding to the pre-compiled header
-
 #include "pch.h"
 bool __fastcall FindPattern(unsigned __int64* pResult, std::string Pattern, int Skips, HMODULE moduleBase)
 {
-    size_t ByteArraySize; // eax
-    char c1; // r12
-    char c2; // r15
-    uintptr_t ModuleHandleA; // rax
-    PIMAGE_NT_HEADERS ntHeaders; // rdx
-    PIMAGE_SECTION_HEADER Section; // Pointer to iterate through sections
-    uintptr_t SectionStartAddr; // r8
-    uintptr_t SectionEnd; // r9
-    bool WasPatternFound = false; // al
-    _Vector_val_1 byteArray; // [rsp+0x28] [rbp-0x71] BYREF
-    Vector_Type1 ByteData{}; // [rsp+0x40] [rbp-0x59] BYREF
-    std::string ByteString; // [rsp+0x90] [rbp-0x9] BYREF
+    struct PatternByte {
+        uint8_t byte;
+        bool isWildcard;
+    };
+    std::vector<PatternByte> patternBytes;
 
-    if (Pattern.size())
-    {
-        std::erase(Pattern, ' ');
-    }
-    std::size_t PatternStringSize = Pattern.size();
-    if (!PatternStringSize || (PatternStringSize & 1) != 0)
-    {
-        return WasPatternFound;
-    }
-    ByteArraySize = PatternStringSize / 2;
-
-    for (std::size_t i = 0; i < ByteArraySize; ++i) {
-        c1 = Pattern[2 * i];
-
-        c2 = Pattern[2 * i + 1];
-        if (c1 == '?' || c2 == '?')
-        {
-            if (c1 != c2)
-            {
-                return WasPatternFound;
-            }
+    std::istringstream iss(Pattern);
+    std::string token;
+    while (iss >> token) {
+        if (token == "?" || token == "??") {
+            patternBytes.push_back({ 0, true });
         }
-        else
-        {
-            ByteString = Pattern.substr(2 * i, 2);
-            ByteData.index = i;
-            ByteData.Byte = std::stoi(ByteString, nullptr, 16);
-            byteArray.push_back(ByteData);
+        else if (token.size() == 2 && std::isxdigit(token[0]) && std::isxdigit(token[1])) {
+            patternBytes.push_back({ static_cast<uint8_t>(std::stoi(token, nullptr, 16)), false });
+        }
+        else {
+            return false;
         }
     }
-	if (moduleBase)
-	{
-		ModuleHandleA = (uintptr_t)moduleBase;
-	}
-	else
-	{
-		// Get the base address of the current module
-		ModuleHandleA = (uintptr_t)GetModuleHandleA(0);
-	}
-    ntHeaders = reinterpret_cast<PIMAGE_NT_HEADERS>(ModuleHandleA + reinterpret_cast<PIMAGE_DOS_HEADER>(ModuleHandleA)->e_lfanew);
-    Section = IMAGE_FIRST_SECTION(ntHeaders);
+    if (patternBytes.empty())
+        return false;
 
-    if (!byteArray.size())
-    {
-        return WasPatternFound;
-    }
+    uintptr_t ModuleHandleA = moduleBase ? (uintptr_t)moduleBase : (uintptr_t)GetModuleHandleA(0);
+    auto* ntHeaders = reinterpret_cast<PIMAGE_NT_HEADERS>(ModuleHandleA + reinterpret_cast<PIMAGE_DOS_HEADER>(ModuleHandleA)->e_lfanew);
+    auto* Section = IMAGE_FIRST_SECTION(ntHeaders);
 
-    // Iterate through all sections
-    for (int i = 0; i < ntHeaders->FileHeader.NumberOfSections; ++i, ++Section)
-    {
-        // Check if the section is executable and readable
-        if ((Section->Characteristics & IMAGE_SCN_MEM_EXECUTE) && (Section->Characteristics & IMAGE_SCN_MEM_READ))
-        {
-            SectionStartAddr = ModuleHandleA + Section->VirtualAddress;
-            SectionEnd = SectionStartAddr + Section->Misc.VirtualSize - ByteArraySize;
+    for (int i = 0; i < ntHeaders->FileHeader.NumberOfSections; ++i, ++Section) {
+        if ((Section->Characteristics & IMAGE_SCN_MEM_EXECUTE) && (Section->Characteristics & IMAGE_SCN_MEM_READ)) {
+            uintptr_t SectionStartAddr = ModuleHandleA + Section->VirtualAddress;
+            uintptr_t SectionEnd = SectionStartAddr + Section->Misc.VirtualSize - patternBytes.size();
 
-            for (; SectionStartAddr < SectionEnd; ++SectionStartAddr)
-            {
-                WasPatternFound = true;
-
-                for (const auto& ByteStruct : byteArray)
-                {
-                    if (*(BYTE*)(ByteStruct.index + SectionStartAddr) != ByteStruct.Byte)
-                    {
-                        WasPatternFound = false;
+            for (; SectionStartAddr < SectionEnd; ++SectionStartAddr) {
+                bool found = true;
+                for (size_t j = 0; j < patternBytes.size(); ++j) {
+                    if (!patternBytes[j].isWildcard &&
+                        *(uint8_t*)(SectionStartAddr + j) != patternBytes[j].byte) {
+                        found = false;
                         break;
                     }
                 }
-
-                if (WasPatternFound)
-                {
-                    if (!Skips)
-                    {
+                if (found) {
+                    if (!Skips) {
                         *pResult = SectionStartAddr;
-                        return true; // Return immediately if the pattern is found
+                        return true;
                     }
                     --Skips;
                 }
             }
         }
     }
-
-    return WasPatternFound;
+    return false;
 }
--- HPCDXTestsCPU/pch.h
+++ HPCDXTestsCPU/pch.h
@@ -1,33 +1,17 @@
-// pch.h: This is a precompiled header file.
-// Files listed below are compiled only once, improving build performance for future builds.
-// This also affects IntelliSense performance, including code completion and many code browsing features.
-// However, files listed here are ALL re-compiled if any one of them is updated between builds.
-// Do not add files here that you will be updating frequently as this negates the performance advantage.
-
 #ifndef PCH_H
 #define PCH_H
 
-// add headers that you want to pre-compile here
 #include "framework.h"
 #include <string>
 #include <vector>
 #include <iostream>
 #include <fstream>
-struct Vector_Type1 {
-    size_t index;
-    BYTE Byte;
-};
-struct VectorValTest1 {
-    Vector_Type1* _Myfirst; // pointer to beginning of array
-    Vector_Type1* _Mylast; // pointer to current end of sequence
-    Vector_Type1* _Myend; // pointer to end of array
-};
-typedef std::vector<Vector_Type1> _Vector_val_1;
+#include <sstream>
 bool __fastcall FindPattern(unsigned __int64* pResult, std::string Pattern, int Skips, HMODULE moduleBase = nullptr);
 
 #include <dxgi.h>
 #include <d3d12.h>
-//#pragma comment(lib, "d3d12.lib")
+#include <dxgi1_4.h>
 
 #if defined _M_X64
 typedef uint64_t uintx_t;
@@ -38,8 +22,6 @@ typedef uint32_t uintx_t;
 #include "imgui/imgui.h"
 #include "imgui/backends/imgui_impl_win32.h"
 #include "imgui/backends/imgui_impl_dx12.h"
-#include <d3d12.h>
-#include <dxgi1_4.h>
 
 #include "namespaces.h"
 
